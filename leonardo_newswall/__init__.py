34af1ff08ab27d760f57f16e25113fab4ee40cdf
Michael Kutý <6du1ro.n@gmail.com>
2015-10-10 02:13:29 +0200

    template improvements

--- leonardo_newswall/__init__.py
+++ leonardo_newswall/__init__.py
@@ -2,6 +2,7 @@
 from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
+from leonardo.apps import app_reverse_lazy
 from .widget import *
 
 default_app_config = 'leonardo_newswall.Config'
@@ -21,6 +22,16 @@ LEONARDO_OPTGROUP = 'Leonardo Newswall'
 LEONARDO_WIDGETS = [
     RecentNewsWidget,
 ]
+
+
+def source_url_override(self):
+    return app_reverse_lazy(
+        'newswall_source_detail',
+        'leonardo_newswall.apps.newswall',
+        kwargs={'slug': self.slug})
+
+LEONARDO_ABSOLUTE_URL_OVERRIDES = {
+    'newswall.source': 'leonardo_newswall.source_url_override'}
 
 
 class Config(AppConfig):
